e77218d981aceb3b1a95ee80726c714155ab0eb9
kinoshjn <kinoshjn@yahoo.co.jp>
2026-06-19 08:03:01 +0900

    Remove users_controller_test and disable test generation

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
 require_relative "boot"
-
 require "rails/all"
-
 Bundler.require(*Rails.groups)
-
 module App
   class Application < Rails::Application
     config.load_defaults 7.2
     config.autoload_lib(ignore: %w[assets tasks])
-
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|html_tag
+    end
+    config.generators do |g|
+      g.test_framework false
+      g.fixture_replacement false
     end
   end
 end
