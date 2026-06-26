a8dcdbcddace1072e5addd28c136b3fd81acfc00
kinoshjn <kinoshjn@yahoo.co.jp>
2026-06-26 15:56:20 +0900

    “login_count計測及びavatar画像アップの実装”

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,8 +5,11 @@ class UserSessionsController < ApplicationController
     @user = User.find_by(email: params[:email])&.authenticate(params[:password])
 
     if @user
+      # ログイン計測用に追加    2026.6/26修正   <user.rb参照>
+      @user.record_login!
+
       session[:user_id] = @user.id
-      redirect_to root_path, success: "ログインしました", status: :see_other
+      redirect_to root_path, status: :see_other
     else
       flash[:danger] = "ログインに失敗しました"
       redirect_to login_path, status: :see_other
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   validates :user_name, presence: true
 
   has_one :aircraft, dependent: :destroy
+
+  # ユーザログイン数計測用  2026.6/26修正
+  def record_login!
+    return if last_login_date == Date.today
+    update!(
+      login_count: login_count.to_i + 1,
+      last_login_date: Date.today
+    )
+  end
 end
